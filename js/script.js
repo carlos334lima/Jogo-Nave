accf149436960168b28cc7fdc5f56bff4d0315f1
Carlos Henrique de Lima <68131444+carlos334lima@users.noreply.github.com>
2020-09-07 10:42:11 -0300

    implementando função de mover o fundo jogo

--- js/script.js
+++ js/script.js
@@ -8,6 +8,27 @@ function start() {
     $("#fundoGame").append("<div id='inimigo1'class='anima2'></div>");
     $("#fundoGame").append("<div id='inimigo2'></div>");
     $("#fundoGame").append("<div id='amigo' class='anima3'></div>");
-}
 
+    //Principais variáveis
+
+    var jogo = {}
+
+    //Game loop
+
+    jogo.timer = setInterval(loop,30);
+    function loop(){
+        movefundo();
+    }                      
+    
+    //Fim do loop
+
+    //Função de movimento do jogo
+
+    function movefundo(){
+        esquerda = parseInt($("#fundoGame").css("background-position"));
+        $("#fundoGame").css ("background-position", esquerda-1);
+    }
+    //Fim da  função movefundo
+}
 //FIM DA FUNÇÃO START
+
